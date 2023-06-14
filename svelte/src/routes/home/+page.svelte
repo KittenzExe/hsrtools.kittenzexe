358ec9196da17de7c2e621c9cff6ab4b041304ac
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-06-14 12:11:51 +0800

    sidebar

---
+++ svelte/src/routes/home/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Sidebar from "../structure/sidebar.svelte"
+</script>
+
+<div id="global">
+  <Sidebar/>
+</div>
+
+<style>
+  #global {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+</style>
---
+++ svelte/src/routes/structure/sidebar.svelte
@@ -0,0 +1,34 @@
+<div id="sidebar-holder">
+  <div id="sidebar-decor">
+    <div id="sidebar-decor-yellow"></div>
+    <div id="sidebar-decor-red"></div>
+  </div>
+  <div id="sidebar-content-holder">
+    <div id="logo-side">
+      <div id="logo"></div>
+      <div id="logotxt"></div>
+    </div>
+  </div>
+</div>
+
+<style>
+  #sidebar-holder {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: max-content;
+    width: 20%;
+    background-color: aqua;
+  }
+  @media screen and (max-width: 600px) {
+    #sidebar-holder {
+      background-color: aquamarine;
+    }
+  }
+  @media screen and (min-width: 1920px) {
+    #sidebar-holder {
+      width: 384px;
+      background-color: blue;
+    }
+  }
+</style>
