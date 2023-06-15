e115361eddbab62e6516df43fe83bc0d735cb5d8
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-06-15 09:38:07 +0800

    new svelte file

---
+++ svelte/src/routes/home/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Sidebar from "../structure/sidebar.svelte"
+</script>
+
+<div id="pagefill">
+  <Sidebar/>
+</div>
+
+<style>
+  #pagefill {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+</style>
