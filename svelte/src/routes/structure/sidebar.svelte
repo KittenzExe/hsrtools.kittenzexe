61c2871a43784326f40018a5bbbc570aa0f59746
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-06-15 11:20:48 +0800

    sidebar DONE!

--- svelte/src/routes/structure/sidebar.svelte
+++ svelte/src/routes/structure/sidebar.svelte
@@ -5,21 +5,32 @@
   </div>
   <div id="sidebar-content-holder">
     <div id="logo-side">
-      <div id="logo"></div>
-      <div id="logotxt"></div>
+      <div id="logo"><img src="./src/images/icon/Pom-Pom_Sticker_11.webp" alt="logo" style="width: 100%;"></div>
+      <div id="logotxt">Honkai: Star Rail Tools</div>
+    </div>
+    <div id="nav-side">
+      <div id="list">
+        <li id="list-li"><a href="">Home</a></li><!-- update before out of beta -->
+        <li id="list-li"><a href="">Characters</a></li><!-- update before out of beta -->
+        <li id="list-li"><a href="">Database</a></li><!-- update before out of beta -->
+        <li id="list-li"><a href="">Calculator</a></li><!-- update before out of beta -->
+        <li id="list-li"><a href="">Settings</a></li><!-- update before out of beta -->
+      </div>
     </div>
   </div>
 </div>
 
 <style>
+  @font-face { font-family: DIN; src: url('./src/fonts/DIN Bold.ttf'); } 
+
   #sidebar-holder {
     margin: 0;
     padding: 0;
     border: 0;
     height: 100vh;
     width: 15vw;
-    background-color: aqua;
     position: sticky;
+    overflow: hidden;
   }
   #sidebar-decor {
     margin: 0;
@@ -28,7 +39,6 @@
     border: 0;
     height: 100vh;
     width: 25%;
-    background-color: orange;
     position: absolute;
   }
   #sidebar-decor-yellow {
@@ -38,7 +48,7 @@
     border: 0;
     height: 100vh;
     width: 30%;
-    background-color: yellow;
+    background-color: #efd1a1;
     position: absolute;
   }
   #sidebar-decor-red {
@@ -47,7 +57,73 @@
     border: 0;
     height: 100vh;
     width: 70%;
-    background-color: red;
+    background-color: #b9141c;
+  }
+  #sidebar-content-holder {
+    margin: 0;
+    padding: 10px;
+    border: 0;
+    width: calc(75% - 20px);
+    height: calc(100vh - 20px);
+    background-color: #fef6ed;
+    position: absolute;
+  }
+  #logo-side {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    width: 100%;
+    height: 296px;
+  }
+  #logo {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    width: 100%;
+  }
+  #logotxt {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    width: 100%;
+    font-size: 200%;
+    font-weight: 500;
+    text-align: center;
+    color: #2e3036;
+  }
+
+  #nav-side {
+    margin: 0;
+    margin-top: 50px;
+    padding: 0;
+    border: 0;
+    width: 100%;
+    height: 60%;
+  }
+  #list {
+    list-style-type: none;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+  }
+  #list-li {
+      overflow: hidden;
+  }
+  #list-li a {
+      display: block;
+      color: #fef6ed;
+      text-align: center;
+      margin: 10px;
+      padding: 20px 30px;
+      text-decoration: none;
+      font-family: DIN;
+      font-size: 20px;
+      font-weight: 500;
+      overflow: hidden;
+      border-width: 5px;
+      border-style: solid;
+      background-color: #2e3036;
+      border-color: #efd1a1;
   }
   
   @media screen and (max-width: calc(288px + 500px)) {
