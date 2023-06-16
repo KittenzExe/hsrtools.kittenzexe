0fbbf3d061b4c07f0b40cb47f22740fbe8a91a6e
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-06-16 12:39:46 +0800

    this is killing me

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-	kit: {
-		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',  // path to public directory
-			assets: 'build',  // path to public directory
-			fallback: null
-		})
-	}
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
+    }
 };
-
-export default config;
