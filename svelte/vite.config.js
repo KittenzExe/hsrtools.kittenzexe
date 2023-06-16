189637fcdcca667603f75c4864b0e9e710d9b298
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-06-16 10:50:26 +0800

    maybe???

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  base: "/hsrtools.kittenzexe/", // change to your repo name
+  plugins: [sveltekit()],
+})
