b90209bc8ebd8465521701a8fe2b63d7b219aa3b
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-06-16 11:49:19 +0800

    move over to pure svelte

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
+    }
+};
