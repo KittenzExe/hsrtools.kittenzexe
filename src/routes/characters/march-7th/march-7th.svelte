d5564f2112834ec595731f8be1c5c9bc646bc71e
Ccencceth <45124495+Ccencceth@users.noreply.github.com>
2023-06-18 04:00:07 +0800

    Added Best Girl

---
+++ src/routes/characters/march-7th/march-7th.svelte
@@ -0,0 +1,334 @@
+<script>
+    import Footer from "../../structure/sidebar.svelte"
+</script>
+
+<div id="content-holder">
+    <div id="content-image-wrap">
+        <img src="../../images/character-art/March7th/Character_March_7th_Portrait.webp" alt="asta" style="height: 650px;">
+    </div>
+    <div id="content-topbar">
+      <div id="content-topbar-title">March 7th <img src="../../images/icon/Icon_4_Stars.webp" alt="4 star" height="35px"> <img src="../../images/elements/Element_Ice.webp" alt="ice" height="35px"> <img src="../../images/path/Path_The_Preservation.webp" alt="preservation" height="35px"></div>
+      <div id="content-topbar-info" style="font-size: 20px;">"Well would you listen to that! I saved everyone without causing any trouble! You're pretty awesome, March 7th!"</div>
+    </div>
+    <div id="content-box-main1">
+        <h3 style="margin-bottom: -5px; padding-bottom: 5px; padding-top: -0px; margin-top: -5px;">Ascensions, Stats and Cost</h3>
+        <div id="content-box-character-stats-container">
+            <div id="content-box-character-stats-container-item">Ascension<br>Phase</div>
+            <div id="content-box-character-stats-container-item">Level</div>
+            <div id="content-box-character-stats-container-item">Base<br>HP</div>
+            <div id="content-box-character-stats-container-item">Base<br>ATK</div>
+            <div id="content-box-character-stats-container-item">Base<br>DEF</div>
+            <div id="content-box-character-stats-container-item">Base<br>SPD</div>
+            <!-- Top bar -->
+            <div id="content-box-character-stats-container-item">0✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">1/20</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">144</div><!--HP-->
+            <div id="content-box-character-stats-container-item">69</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">78</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">0✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">20/20</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">280</div><!--HP-->
+            <div id="content-box-character-stats-container-item">135</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">152</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">1✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">20/30</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">338</div><!--HP-->
+            <div id="content-box-character-stats-container-item">163</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">183</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">1✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">30/30</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">410</div><!--HP-->
+            <div id="content-box-character-stats-container-item">198</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">222</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+            
+            <div id="content-box-character-stats-container-item">2✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">30/40</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">468</div><!--HP-->
+            <div id="content-box-character-stats-container-item">226</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">253</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">2✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">40/40</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">540</div><!--HP-->
+            <div id="content-box-character-stats-container-item">261</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">292</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+            
+            <div id="content-box-character-stats-container-item">3✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">40/50</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">597</div><!--HP-->
+            <div id="content-box-character-stats-container-item">288</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">323</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">3✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">50/50</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">669</div><!--HP-->
+            <div id="content-box-character-stats-container-item">323</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">362</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+            
+            <div id="content-box-character-stats-container-item">4✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">50/60</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">727</div><!--HP-->
+            <div id="content-box-character-stats-container-item">351</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">393</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">4✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">60/60</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">799</div><!--HP-->
+            <div id="content-box-character-stats-container-item">386</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">432</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+            
+            <div id="content-box-character-stats-container-item">5✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">60/70</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">856</div><!--HP-->
+            <div id="content-box-character-stats-container-item">414</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">464</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">5✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">70/70</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">928</div><!--HP-->
+            <div id="content-box-character-stats-container-item">448</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">503</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+            
+            <div id="content-box-character-stats-container-item">6✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">70/80</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">986</div><!--HP-->
+            <div id="content-box-character-stats-container-item">476</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">534</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+    
+            <div id="content-box-character-stats-container-item">6✦</div><!--ASCENSION-->
+            <div id="content-box-character-stats-container-item">80/80</div><!--LEVEL-->
+            <div id="content-box-character-stats-container-item">1,058</div><!--HP-->
+            <div id="content-box-character-stats-container-item">511</div><!--ATK-->
+            <div id="content-box-character-stats-container-item">473</div><!--DEF-->
+            <div id="content-box-character-stats-container-item">101</div><!--SPD-->
+        </div>
+        <div id="content-box-character-stats-container2">
+            <div id="content-box-character-stats-container-item">Ascension<br>Cost</div>
+            <div id="content-box-character-stats-container-item"></div>
+            <div id="content-box-character-stats-container-item"></div>
+            <div id="content-box-character-stats-container-item"></div>
+            <!-- Top bar -->
+    
+            <div id="content-box-character-stats-container-item"><p style="height: 18px;"></p></div>
+            <div id="content-box-character-stats-container-item"></div>
+            <div id="content-box-character-stats-container-item"></div>
+            <div id="content-box-character-stats-container-item"></div>
+    
+            <div id="content-box-character-stats-container-item">0 to 1</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Credit.webp" alt="credit" style="height: 45px;">3,200</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Thief's_Instinct.webp" alt="thiefs-instinct" style="height: 45px;">4</div>
+            <div id="content-box-character-stats-container-item"></div>
+    
+            <div id="content-box-character-stats-container-item">1 to 2</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Credit.webp" alt="credit" style="height: 45px;">6,400</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Thief's_Instinct.webp" alt="thiefs-instinct" style="height: 45px;">8</div>
+            <div id="content-box-character-stats-container-item"></div>
+            
+            <div id="content-box-character-stats-container-item">2 to 3</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Credit.webp" alt="credit" style="height: 45px;">12,800</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Horn_of_Snow.webp" alt="chitin" style="height: 45px;">2</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Usurper's_Scheme.webp" alt="usurpers-scheme" style="height: 45px;">5</div>
+            
+            <div id="content-box-character-stats-container-item">3 to 4</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Credit.webp" alt="credit" style="height: 45px;">32,000</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Horn_of_Snow.webp" alt="chitin" style="height: 45px;">5</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Usurper's_Scheme.webp" alt="usurpers-scheme" style="height: 45px;">8</div>
+            
+            <div id="content-box-character-stats-container-item">4 to 5</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Credit.webp" alt="credit" style="height: 45px;">64,000</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Horn_of_Snow.webp" alt="chitin" style="height: 45px;">15</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Conqueror's_Will.webp" alt="conquerors-will" style="height: 45px;">5</div>
+            
+            <div id="content-box-character-stats-container-item">5 to 6</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Credit.webp" alt="credit" style="height: 45px;">128,000</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Horn_of_Snow.webp" alt="chitin" style="height: 45px;">28</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/icon/Item_Conqueror's_Will.webp" alt="conquerors-will" style="height: 45px;">7</div>
+        </div>
+        <h3 style="margin-bottom: -5px; padding-bottom: 5px; padding-top: 0px; margin-top: 5px;">Best Lightcones</h3>
+        <div id="content-box-character-stats-container3">
+            <div id="content-box-character-stats-container-item2">Best</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/lightcones/Light_Cone_Moment_of_Victory.webp" alt="lightcone" style="height: 35px;"></div>
+            <div id="content-box-character-stats-container-item2">Moment of Victory</div>
+    
+            <div id="content-box-character-stats-container-item2">2nd Best</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/lightcones/Light_Cone_Day_One_of_My_New_Life.webp" alt="lightcone" style="height: 35px;"></div>
+            <div id="content-box-character-stats-container-item2">Day One of My New Life</div>
+            
+            <div id="content-box-character-stats-container-item2">3rd Best</div>
+            <div id="content-box-character-stats-container-item"><img src="../images/lightcones/Light_Cone_This_Is_Me!.webp" alt="lightcone" style="height: 35px;"></div>
+            <div id="content-box-character-stats-container-item2">This Is Me!</div>
+        </div>
+    </div>
+    <div id="content-box-listing" style="height: 380px;">
+        <h1 style="margin-bottom: -5px; padding-bottom: 5px; padding-top: -0px; margin-top: -5px;">Abilities</h1>
+        <h3 style="margin-bottom: -5px; padding-bottom: 5px; padding-top: -0px; margin-top: -5px;"><img src="../images/character-art/March7th/Skill_Frigid_Cold_Arrow.webp" alt="basic-atk" style="height: 40px;"> Basic ATK (Single Target)  | Frigid Cold Arrow | Deals Ice DMG equal to 50%–130% of March 7th's ATK to a single enemy.<br><br></h3> 
+        <h3 style="margin-bottom: -5px; padding-bottom: 5px; padding-top: -0px; margin-top: -5px;"><img src="../images/character-art/March7th/Skill_The_Power_of_Cuteness.webp" alt="skill" style="height: 40px;"> Skill (Defence)  | The Power of Cuteness | Provides a single ally with a Shield that can absorb DMG equal to 38%–66.5% of March 7th's DEF plus 190–973.75 for 3 turn(s). If the ally's current HP percentage is 30% or higher, greatly increases the chance of enemies attacking that ally.<br><br></h3>
+        <h3 style="margin-bottom: -5px; padding-bottom: 5px; padding-top: -0px; margin-top: -5px;"><img src="../images/character-art/March7th/Skill_Glacial_Cascade.webp" alt="ultimate" style="height: 40px;"> Ultimate (AoE)  | Glacial Cascade | Deals Ice DMG equal to 90%–180% of March 7th's ATK to all enemies. Hit enemies have a 50% base chance to be Frozen for 1 turn(s). While Frozen, enemies cannot take action and will receive Additional Ice DMG equal to 30%–75% of March 7th's ATK at the beginning of each turn.<br><br></h3> 
+    </div>
+</div>
+
+<Footer/>
+
+<style>
+    @font-face { font-family: DIN; src: url('../../fonts/DIN Bold.ttf'); } 
+  
+  #content-holder {
+    margin: 0;
+    margin-left: 15vw;
+    padding: 0;
+    border: 0;
+    min-height: 100vh;
+    flex: 1;
+    width: calc(100vw - 15vw - 17.5px);
+    background-color: #2e3036;
+    float: left;
+  }
+
+  /*FROM OLD CSS*/
+
+  #content-topbar {
+        width: 100%;
+        height: 150px;
+        padding: 0;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        position: relative;
+        overflow: hidden;
+        align-items: center;
+    }
+    #content-topbar-title {
+        width: 100%;
+        height: 80px;
+        padding: 0;
+        top: 0;
+        left: 10px;
+        bottom: 0;
+        position: absolute;
+        overflow: hidden;
+        font-family: DIN;
+        font-size: 75px;
+        font-weight: bold;
+        color: #b9141c;
+        display: inline
+    }
+    #content-topbar-info {
+        width: 100%;
+        height: 80px;
+        padding: 0;
+        top: 80px;
+        left: 15px;
+        bottom: 0;
+        position: absolute;
+        overflow: hidden;
+        font-family: DIN;
+        font-size: 35px;
+        font-weight: bold;
+        color: #efd1a1;
+    }
+    #content-image-wrap {
+        width: flex;
+        height: 700px;
+        margin-top: 100px;
+        margin-left: 10px;
+        padding-top: 5%;
+        align-items: center;
+        overflow: hidden;
+        position: absolute;
+    }
+
+    #content-box-main1 {
+        width: 65%;
+        height: 75%;
+        margin-left: 500px;
+        padding: 10px;
+        top: 20px;
+        left: -10px;
+        bottom: 10px;
+        position: relative;
+        overflow: hidden;
+        background-color: #43454e;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #efd1a1;
+        border-top-right-radius: 50px;
+        border-bottom-left-radius: 50px;
+        font-family: DIN;
+        font-size: 20px;
+        color: #fef6ed;
+    }
+
+    #content-box-listing {
+        width: 95%;
+        margin-left: 30px;
+        margin-bottom: 100px;
+        padding: 10px;
+        top: 60px;
+        left: -10px;
+        bottom: 10px;
+        overflow: hidden;
+        position: relative;
+        background-color: #43454e;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #efd1a1;
+        border-top-right-radius: 50px;
+        border-bottom-left-radius: 50px;
+        font-family: DIN;
+        font-size: 20px;
+        color: #fef6ed;
+    }
+
+    #content-box-character-stats-container {
+        display: grid;
+        width: 50%;
+        grid-template-columns: auto auto auto auto auto auto;
+        padding-top: 5px;
+        font-size: 10px;
+        position: absolute;
+    }
+    #content-box-character-stats-container2 {
+        display: grid;
+        margin-left: 51%;
+        width: 45%;
+        grid-template-columns: auto auto auto auto;
+        padding-top: 5px;
+        font-size: 10px;
+    }
+    #content-box-character-stats-container3 {
+        display: grid;
+        width: 45%;
+        grid-template-columns: auto auto auto;
+        padding-top: 5px;
+        font-size: 15px;
+    }
+    #content-box-character-stats-container-item {
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        padding: 3px;
+        font-size: 15px;
+        text-align: center;
+    }
+    #content-box-character-stats-container-item2 {
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        padding: 3px;
+        font-size: 20px;
+        text-align: center;
+        padding-top: 10px;
+    }
+</style>
